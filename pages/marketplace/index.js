8eb6b98f6c9eb56a8b95f8c7adf5cf5e98915863
MZ Marshall <mzmarshall1278@gmail.com>
2022-04-09 23:30:31 +0100

    updated some styles

--- pages/marketplace/index.js
+++ pages/marketplace/index.js
@@ -5,8 +5,8 @@ const MarketPlace = ({data}) => {
         <div className='min-h-screen overflow-x-hidden bg-repeat-y bg-cover'>
                 
                 <div className="w-2/3 mx-auto flex mt-4">
-                    <button className="w-1/4 bg-white text-black duration-300  hover:bg-[#ffffff31] hover:text-white outline-none text-center font-bold">Search</button>
-                    <input type="text" placeholder="Search by club" className="w-3/4 outline-none py-3 px-2 border ml-4 border-white rounded-sm"/>
+                    <button className="w-1/4 bg-transparent border rounded-sm text-white duration-300  hover:bg-[#ffffff31] hover:text-white outline-none text-center font-bold">Search</button>
+                    <input type="text" placeholder="Search by club" className="w-3/4 outline-none py-3 px-2 border ml-4 border-white text-white rounded-sm bg-transparent"/>
                 </div>
 
                 <div className="grid gap-3 grid-cols-4 mt-12 w-5/6  mx-auto">
