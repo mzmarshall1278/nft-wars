575f6d1f41924f1185e0a1469d36c7e4874ba31f
MZ Marshall <mzmarshall1278@gmail.com>
2023-10-05 22:39:50 +0100

    comment

--- components/NFT.jsx
+++ components/NFT.jsx
@@ -1,12 +1,13 @@
 import Link from 'next/link';
 import {BsImage, BsCameraVideo, BsMusicNote} from 'react-icons/bs'
 import Chip from './chip';
+import Download from './download';
 
 
 const NFT = ({card}) => {
     const {url, title, price, club, type, id} = card;
     return ( 
-        <Link href={`/marketplace/${id}`}>
+        // <Link href={`/marketplace/${id}`}>
             <div className="border-4 border-[#5e8dfc69] rounded-lg transform hover:-translate-y-2 duration-100 cursor-pointer">
                         <div
                             className=" bg-cover h-72 bg-center relative rounded-t-lg"
@@ -33,10 +34,15 @@ const NFT = ({card}) => {
                             `}>{title}</h1>
                             <h1 className='my-2'>{price} Near</h1>
                             <Chip club={club}/>
-                            <button className='w-full mx-auto text-center mt-5 border-2 rounded-lg py-2 hover:bg-[rgba(22,22,22,0.7)] duration-300 hover:text-white' >Buy</button>
+                            <Link href={`/marketplace/${id}`} >
+                                <button className='block w-full mx-auto text-center mt-5 border-2 rounded-lg py-2 hover:bg-[rgba(22,22,22,0.7)] duration-300 hover:text-white'>
+                                    Buy
+                                </button>
+                                </Link>
+                            {/* <Download dataurl="https://www.mintbase.io/_next/image?url=https%3A%2F%2Farweave.net%2FGn4KESJCWPtbvCCHDjVMQuOyE5qyXrvKsFVfnwZFGUQ&w=1920&q=75" filename="test"/> */}
                         </div>
             </div>
-        </Link>
+        // </Link>
      );
 }
  
